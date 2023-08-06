fc6b9e79f71c5d94f28b97d4aa24df1d56a4185a
MANAS JYOTI PACHANI <mjp787828@gmail.com>
2023-08-06 16:05:17 +0530

    first commit

---
+++ src/components/Expenses/ExpenseDate.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./ExpenseDate.css";
+
+const ExpenseDate = (props) => {
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const month = monthNames[props.date.getMonth() - 1];
+  const day = props.date.getDate();
+  const year = props.date.getFullYear();
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+};
+
+export default ExpenseDate;
---
+++ src/components/UI/Card.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Card.css";
+
+const Card = (props) => {
+  const cardClass = "card " + props.className;
+  return <div className={cardClass}>{props.children}</div>;
+};
+
+export default Card;
